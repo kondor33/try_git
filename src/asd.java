427dbd10c7655d14a75304dc89a7ab4d418c64c9
LAB702-13 <LAB702-13@LAB702-13.laboratorium.zie>
2014-11-12 13:29:37 +0100

    Laboratorium 6
    Zadanie wykonane poprawnie
    
    Maciej Wojciechowski

---
+++ src/asd.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author LAB702-13
+ */
+public class asd {
+    public void lab6 ()
+    {
+        System.out.println("Ala ma kota ");
+        
+    }
+}
